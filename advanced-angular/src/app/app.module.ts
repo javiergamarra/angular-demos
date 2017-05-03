f8a8c7c926977120868a24c02b41552cad9c2150
Javier Gamarra <nhpatt@gmail.com>
2017-05-03 20:08:45 +0200

    add new component talks

--- advanced-angular/src/app/app.module.ts
+++ advanced-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TalksComponent } from './talks/talks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TalksComponent
   ],
   imports: [
     BrowserModule,
