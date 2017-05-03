b00adaa0293413977f83f9a99a0bb5ddb6820c81
Javier Gamarra <nhpatt@gmail.com>
2017-05-03 20:22:33 +0200

    play with interpolation

--- advanced-angular/src/app/talks/talks.component.ts
+++ advanced-angular/src/app/talks/talks.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TalksComponent implements OnInit {
 
+  talks: Array<any>;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.talks = [
+      {name: 'Angular'}
+    ];
   }
 
 }
