18de7866884940d1114ec54908f6db5d9ca89e73
Javier Gamarra <nhpatt@gmail.com>
2017-05-03 21:30:03 +0200

    add if and else from angular4

--- advanced-angular/src/app/talks.service.ts
+++ advanced-angular/src/app/talks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class TalksService {
@@ -9,7 +10,7 @@ export class TalksService {
   }
 
   getTalks() {
-    return this.http.get('http://data.agenda.wedeploy.io/talks').map(res => res.json());
+    return this.http.get('http://data.agenda.wedeploy.io/talks').delay(2000).map(res => res.json());
   }
 
 }
