a4e214bce9b3cd5cf35ae28083c8e32224d7c4a5
Javier Gamarra <nhpatt@gmail.com>
2017-05-04 00:53:07 +0200

    example using flatmap

--- advanced-angular/src/app/talks/talks.component.ts
+++ advanced-angular/src/app/talks/talks.component.ts
@@ -24,15 +24,18 @@ export class TalksComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.talks$ =
-      Observable.merge(Observable.of(''),
-        Observable.fromEvent(this.search.nativeElement, 'keyup')
-          .map((e: any) => e.target.value)
-          .filter(text => text.length > 2))
-        .do(x => console.log(x))
-        .debounceTime(700)
-        .distinctUntilChanged()
-        .switchMap(x => this.talkService.getTalks(x));
+
+    Observable.from([1, 2, 3]).flatMap(x => Observable.from([x, x * 2])).subscribe(x => console.log(x));
+
+    // this.talks$ =
+    //   Observable.merge(Observable.of(''),
+    //     Observable.fromEvent(this.search.nativeElement, 'keyup')
+    //       .map((e: any) => e.target.value)
+    //       .filter(text => text.length > 2))
+    //     .do(x => console.log(x))
+    //     .debounceTime(700)
+    //     .distinctUntilChanged()
+    //     .switchMap(x => this.talkService.getTalks(x));
   }
 
   onClicked($event, element) {
