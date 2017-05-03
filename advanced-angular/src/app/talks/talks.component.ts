cb4bd5d48a476c5b515c88514cdf3171e7029126
Javier Gamarra <nhpatt@gmail.com>
2017-05-04 01:16:37 +0200

    publish subject

--- advanced-angular/src/app/talks/talks.component.ts
+++ advanced-angular/src/app/talks/talks.component.ts
@@ -25,24 +25,25 @@ export class TalksComponent implements OnInit {
 
   ngOnInit() {
 
-    const obsA = Observable.interval(1000).map(x => 'a');
-    const obsB = Observable.interval(1000).map(x => 'b');
+    // this.talks$ = this.doNetworkRequest();
+    const obsA = this.doNetworkRequest();
+    const obsB = this.doNetworkRequest();
 
-    // obsA.combineLatest(obsB).subscribe(x => console.log(x));
-    // obsA.concat(obsB).subscribe(x => console.log(x));
-    // obsA.merge(obsB).subscribe(x => console.log(x));
-    // obsA.race(obsB).subscribe(x => console.log(x));
-    // obsA.zip(obsB).subscribe(x => console.log(x));
+    obsA.subscribe(x => console.log(x));
+    obsB.subscribe(x => console.log(x));
+    obsA.connect();
+  }
 
-      // this.talks$ =
-    //   Observable.merge(Observable.of(''),
-    //     Observable.fromEvent(this.search.nativeElement, 'keyup')
-    //       .map((e: any) => e.target.value)
-    //       .filter(text => text.length > 2))
-    //     .do(x => console.log(x))
-    //     .debounceTime(700)
-    //     .distinctUntilChanged()
-    //     .switchMap(x => this.talkService.getTalks(x));
+  private doNetworkRequest() {
+    return Observable.merge(Observable.of(''),
+      Observable.fromEvent(this.search.nativeElement, 'keyup')
+        .map((e: any) => e.target.value)
+        .filter(text => text.length > 2))
+      .do(x => console.log(x))
+      .debounceTime(700)
+      .distinctUntilChanged()
+      .switchMap(x => this.talkService.getTalks(x))
+      .publish();
   }
 
   onClicked($event, element) {
