e6b4461f0d40a91afb36edd40bb70a731c167c82
Javier Gamarra <nhpatt@gmail.com>
2017-05-04 03:57:46 +0200

    my first structural directive

--- advanced-angular/src/app/talk/talk.directive.ts
+++ advanced-angular/src/app/talk/talk.directive.ts
@@ -1,17 +1,35 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  HostBinding,
+  HostListener,
+  Input,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
 
 @Directive({
   selector: '[appTalk]'
 })
-export class TalkDirective {
+export class TalkDirective implements AfterViewInit {
 
   @Input() appTalk;
+
+  ngAfterViewInit(): void {
+    this.viewContainerRef.createEmbeddedView(this.templateRef);
+    this.viewContainerRef.createEmbeddedView(this.templateRef);
+  }
 
   @HostBinding() get innerText() {
     return this.appTalk;
   }
 
-  constructor() {
+  @HostListener('click') onClick() {
+    console.log('clicked!');
   }
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainerRef: ViewContainerRef) {
+  }
+
 
 }
