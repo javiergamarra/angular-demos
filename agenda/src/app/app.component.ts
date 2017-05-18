3429b9a53770b05342d91cffadbc26cc83881372
Javier Gamarra <nhpatt@gmail.com>
2017-05-18 02:57:01 +0200

    router params

--- agenda/src/app/app.component.ts
+++ agenda/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private router: Router) {
+  }
+
+  route(id) {
+    this.router.navigate(['talk', id])
+    console.log(id);
   }
 
 }
